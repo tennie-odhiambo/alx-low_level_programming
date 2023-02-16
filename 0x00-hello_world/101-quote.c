aff014c3043074b44d2506ee345b6c3c4fe50500
tennie-odhiambo <tennieodhiambo@gmail.com>
2023-02-16 11:21:07 -0800

    Hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
+
+include <stdio.h>
 include <unistd.h>
 
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line,to the standard error
- * Return: Always 1
-*/
+ * Return: Always 1 (success)
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
