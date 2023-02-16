121632db9871e6d7eadb9e1f2da7a37092cd5fc5
tennie-odhiambo <tennieodhiambo@gmail.com>
2023-02-16 11:07:16 -0800

    C-Hello,World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 include < stdio.h >
 include < unistd.h >
 /**
- * main -C programme that prints a line to the standard error
- * Return:1
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line,to the standard error
+ * Return: Always 1
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
